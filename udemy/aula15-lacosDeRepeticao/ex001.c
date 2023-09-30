7f645ab7421510051049620e23492c10eb68dfe9
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-30 14:20:16 -0300

    aula15-ex001

---
+++ udemy/aula15-lacosDeRepeticao/ex001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int n = 1;
+
+    while (n < 10)
+    {
+        printf("%i\n", n);
+        n++;
+    }
+
+    return 0;
+}
