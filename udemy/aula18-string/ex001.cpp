a8fdff04c7eb06a9dcfabf906085febadbf1a64f
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-09-30 17:50:40 -0300

    aula18-c++

---
+++ udemy/aula18-string/ex001.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nome;
+
+    cout << "Digite um nome \n";
+
+    cin >> nome;
+
+    cout << "O nome Digitado foi: " << nome;
+
+    return 0;
+}
