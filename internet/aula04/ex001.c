ff8ce0a01fa89eb0a2c7143c1d1672c79dcc6a3b
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-10-07 15:22:38 -0300

    aula04 - ex001

---
+++ internet/aula04/ex001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int num = 10;
+
+    while (num >= 0)
+    {
+        printf("%i \n", num);
+
+        num--;
+    }
+
+    return 0;
+}
