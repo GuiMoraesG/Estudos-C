753c549ffe7bbe7b296cc14ed1b7d9dacb1d1005
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2023-10-07 11:26:27 -0300

    aula01 - ex001

--- internet/aula01/ex001.c
+++ internet/aula01/ex001.c
@@ -2,7 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hellow, World!");
-    
+    int a = 10, b = 1, c = 8;
+
+    printf("Hellow, World: %i %i %i\n", a, b, c);
+    printf("Hellow, World: %s\n", "teste");
+
     return 0;
 }
